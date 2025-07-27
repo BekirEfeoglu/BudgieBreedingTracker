e48845603b4b364bc8c73172e664d9ae052e5ab2
BekirEfeoglu <befeoglu016@gmail.com>
2025-07-27 15:01:05 +0300

    BudgieBreedingTracker

--- add-egg-number-to-chicks.sql
+++
@@ -1,9 +0,0 @@
--- Add egg_number column to chicks table
-ALTER TABLE public.chicks 
-ADD COLUMN IF NOT EXISTS egg_number INTEGER;
-
--- Add comment to explain the column
-COMMENT ON COLUMN public.chicks.egg_number IS 'The number of the egg this chick hatched from';
-
--- Create index for better performance
-CREATE INDEX IF NOT EXISTS idx_chicks_egg_number ON public.chicks(egg_number) WHERE egg_number IS NOT NULL; 
